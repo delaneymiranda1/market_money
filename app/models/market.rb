92c1e0b231f92f4a3cf61dee8fd6ca7465034d4b
Miranda Delaney <delaney.miranda1@gmail.com>
2023-12-05 12:29:17 -0700

    add models and relationships

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,4 +1,7 @@
 class Market < ApplicationRecord
+  has_many :market_vendors, dependent: :destroy
+  has_many :vendors, through: :market_vendors
 
+  validates :name, :street, :city, :county, :state, :zip, :lat, :lon, presence: true
   
 end
