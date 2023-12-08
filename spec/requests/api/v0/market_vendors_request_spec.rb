ec18b625e9b34ff2b06cdce433059e10119c6290
Miranda Delaney <delaney.miranda1@gmail.com>
2023-12-07 20:45:38 -0700

    partial complete on market vendor creation, market cant be blank error

--- spec/requests/api/v0/market_vendors_request_spec.rb
+++ spec/requests/api/v0/market_vendors_request_spec.rb
@@ -79,4 +79,42 @@ describe "Market Vendors" do
 
     expect(new_vendor_data[:relationships][:markets][:data].last[:id].to_i).to eq(market.id)
   end
+
+  it 'gets error for creating a market vendor if invalid id passed in' do 
+    vendor = create(:vendor)
+    market_vendor_params = ({
+      market_id: 1,
+      vendor_id: vendor.id
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("404")
+    expect(data[:errors].first[:title]).to eq("Validation failed: Market must exist")
+  
+    vendor = create(:vendor)
+    market_vendor_params = ({
+      market_id: market.id,
+      vendor_id: 1
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("404")
+    expect(data[:errors].first[:title]).to eq("Validation failed: Vendor must exist")
+  end
+
+  
 end
